d651b304e8052a4c915a8e7e4406721ca5d7112d
Meghna <meganraj251@gmail.com>
2022-03-18 17:48:25 +0530

    Time: 1 ms (98.98%), Space: 39.5 MB (69.22%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,5 +1,8 @@
 class Solution {
     public int mySqrt(int x) {
-        return((int)Math.sqrt(x));
+        long r = x;
+        while(r*r>x)
+            r = (r+x/r)/2;
+        return (int)r;
     }
 }
