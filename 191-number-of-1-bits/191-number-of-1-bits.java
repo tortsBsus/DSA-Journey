7d2ecb5dedf57aaa01b2d2df5810c81833b34a54
Meghna <meganraj251@gmail.com>
2022-03-23 18:26:15 +0530

    Time: 1 ms (82.04%), Space: 41.3 MB (45.21%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.java
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -2,10 +2,10 @@ public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
          int count =0;
-        while(n!=0)
+        while(n!=0) //cant be >0 because java has no unsigned int
         {
-            count += n & 1;
-            n = n>>>1;
+            count += n & 1; //& with 1 will give us if the rightmost bit is 1
+            n = n>>>1; //shifts it right by 1 >> is sign change
         }
         return count;
         
