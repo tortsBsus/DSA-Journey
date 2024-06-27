12e5a70a081c9687cbbfa01801cc9a7d08302b53
Meghana Rathanraj <meganraj251@gmail.com>
2024-06-27 07:17:26 +0530

    Time: 83 ms (69.5%), Space: 64 MB (83.22%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.js
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.js
@@ -2,21 +2,14 @@
  * @param {number[][]} edges
  * @return {number}
  */
-var findCenter = function(edges) {
-    let answer = 0;
-    let initiatorsCount = {};
+var findCenter = function (edges) {
 
-    for(let edge of edges) {
-        if(edge[0] in initiatorsCount) initiatorsCount[edge[0]] += 1;
-        else initiatorsCount[edge[0]] = 1;
+    //   Because it is a star graph
+    let edgeA = edges[0];
+    let edgeB = edges[1];
+    if (edgeA[0] == edgeB[0]) return edgeA[0];
+    else if (edgeA[1] == edgeB[0]) return edgeA[1];
+    else if (edgeA[0] == edgeB[1]) return edgeA[0];
+    else return edgeA[1];
 
-          if(edge[1] in initiatorsCount) initiatorsCount[edge[1]] += 1;
-        else initiatorsCount[edge[1]] = 1;
-    }
-    
-    for( let initiator in initiatorsCount){
-        if(initiatorsCount[initiator] > answer ) answer = initiator;
-    }
-
-    return answer;
 };
